0436f9c7be4ac3a9a4a919cb01e2d63565c88f77
thejasp <salinaj2@miamoh.edu>
2022-10-10 22:32:48 -0400

    Created simple thread concept

--- fleropp_fpm/src/fleropp_fpm.cpp
+++ fleropp_fpm/src/fleropp_fpm.cpp
@@ -1,13 +1,25 @@
 #include "FCGIHandler.hpp"
-
+#include <thread>
+#include <vector>
 using namespace fleropp_fpm;
 
-int main (int argc, char **argv) {
+void handler_job1 (const unsigned int port, const std::string endpoint, const std::string basename) {
     // Construct a handler that will listen on TCP port 50001
-    FCGIHandler handler{50001};
+    FCGIHandler handler{port};
     // Add some endpoint mappings
-    handler.add_endpoint("/hello.elf", "HelloWorldView");
-    handler.add_endpoint("/time.elf", "TimeView");
+    handler.add_endpoint(endpoint, basename);
     // Start accepting connections (blocking)
     handler.accept();
+}
+
+
+int main (int argc, char **argv) {
+    auto t1 = std::thread(handler_job1, 50001, "/hello.elf", "HelloWorldView");
+    auto t2 = std::thread(handler_job1, 50002, "/time.elf", "TimeView");
+    std::vector<std::thread> vecThread; 
+    vecThread.push_back(std::move(t1));
+    vecThread.push_back(std::move(t2));
+    for (auto& thread : vecThread) {
+        thread.join();
+    }
 }
