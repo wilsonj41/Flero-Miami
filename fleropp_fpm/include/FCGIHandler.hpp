daf06ff9c3519ff47e87020764164c6dfb9e4111
salinaj2 <salinaj2@miamioh.edu>
2022-10-11 19:42:44 -0400

    Documentation branch

--- fleropp_fpm/include/FCGIHandler.hpp
+++ fleropp_fpm/include/FCGIHandler.hpp
@@ -12,9 +12,30 @@
 namespace fleropp_fpm {
     class FCGIHandler {
       public:
+        /***
+         * Constructor
+         * 
+         * \param unix_socket Path to unix socket file
+         * \param backlog The number of incoming requests in the background
+         */
         FCGIHandler(const std::string &unix_sock, const unsigned int backlog = 512);
+        /***
+         * Constructor
+         * 
+         * \param tcp_sock Port number assigned to socket
+         * \param backlog The number of incoming requests in the background
+         */
         FCGIHandler(const unsigned int tcp_sock, const unsigned int backlog = 512);
+        /***
+         * Starts event loop and accepts incoming requests
+         */
         void accept();
+
+        /***
+         * Maps an endpoint to a Compilation Unit
+         * \param path Endpoint path 
+         * \param basename Basename of Compilation Unit
+         */ 
         void add_endpoint(const std::string &path, const std::string &basename);
       
       private:
