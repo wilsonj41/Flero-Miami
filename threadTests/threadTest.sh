6af80b7234be743abf00b19a51f4d799d9522257
salinaj2 <salinaj2@miamioh.edu>
2022-10-23 17:30:32 -0400

    Curl now shows response output

--- threadTests/threadTest.sh
+++ threadTests/threadTest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-xargs -I % -P 5 curl -w "@curl-format.txt" -o /dev/null -s "http://localhost:8080/hello.elf" < <(printf '%s\n' {1..5})
+xargs -I % -P 5 curl -w "@curl-format.txt" -s "http://localhost:8080/hello.elf" < <(printf '%s\n' {1..5})
 
 #xargs -I % -P 5 
