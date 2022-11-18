42d9cf1c36c14126f8c27dad54ab5448f9089995
linr5 <linr5@miamioh.edu>
2022-11-18 13:45:00 -0500

    Added three test pages using HTMLStream class

---
+++ docker/httpd_container/public-html/IframeExampleView.hpp
@@ -0,0 +1,13 @@
+#ifndef IFRAME_EXAMPLE_VIEW_HPP
+#define IFRAME_EXAMPLE_VIEW_HPP
+
+#include "IView.hpp"
+
+class IframeExampleView : public IView {
+  public:
+    IframeExampleView() = default;
+    ~IframeExampleView() = default;
+    void generate() override;
+};
+
+#endif /* IFRAME_EXAMPLE_VIEW_HPP */
