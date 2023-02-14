6a11a49629cc390caefb129dd1eacfaef97a2f69
Raymond Lin <linr5.miamioh.edu>
2023-02-13 19:57:12 -0500

    Add case insensitivity to header keys

--- fleropp_fpm/include/RequestData.hpp
+++ fleropp_fpm/include/RequestData.hpp
@@ -17,7 +17,6 @@ namespace fleropp_io {
             RequestData(const std::string& type, std::stringstream& stream, const std::string& query_string_content);
             void parse();
             std::string get_header(const std::string& key) const;
-            const std::string& operator[](const std::string& key) const;
             QueryString get_query_string() const;
         private:
             std::string m_request_type;
--- fleropp_fpm/src/RequestData.cpp
+++ fleropp_fpm/src/RequestData.cpp
@@ -1,4 +1,5 @@
 #include "RequestData.hpp"
+#include <locale>
 
 namespace fleropp_io {
     const std::regex RequestData::header_regex{header_pattern};
@@ -17,19 +18,20 @@ namespace fleropp_io {
             } else {
                 std::smatch matches;
                 std::regex_search(header_line,matches,header_regex);
-                m_header.emplace(std::make_pair(matches[1],matches[2]));
+                std::string key = matches[1].str();
+                std::string lower_key;
+                std::transform(std::cbegin(key),std::cend(key),std::begin(lower_key),::tolower);
+                m_header.emplace(std::make_pair(lower_key,matches[2]));
             }
         }
     }
 
     std::string RequestData::get_header(const std::string& key) const {
-        const auto value = m_header.find(key);
+        std::string lower_key;
+        std::transform(std::begin(key),std::end(key),std::begin(lower_key),::tolower);
+        const auto value = m_header.find(lower_key);
         return (value != std::end(m_header)) ? value->second : "";
     };
-
-    const std::string& RequestData::operator[](const std::string& key) const {
-        return m_header.at(key);
-    }
 
     QueryString RequestData::get_query_string() const {
         return m_query_string;
