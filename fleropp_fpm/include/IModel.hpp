da92c48db33b21f5c29718858099d5eabafdb7ff
linr5 <linr5@miamioh.edu>
2023-03-15 17:55:06 +0000

    Rework IModel to CRTP pattern, Reword function name in Driver, and Make new Model Abstract class

--- fleropp_fpm/include/IModel.hpp
+++ fleropp_fpm/include/IModel.hpp
@@ -1,22 +1,81 @@
 #ifndef IMODEL_HPP
 #define IMODEL_HPP
 
+#include <string>
+#include <unordered_map>
+
+template <class Derived>
 class IModel {
     public:
-        std::string table_name;
-        std::string primary_key_name;
+        void find(int id) {
+            static_cast<Derived*>(this)->find_impl(id);
+        }
 
-        virtual void find(int id) = 0;
-        virtual void find(xonst std::string& query) = 0;
-        template<typename T>
-        virtual T get(const std::string& col_name, T default_value) = 0;
-        virtual bool set(const std::string& col_name, const std::string& value) = 0;
-        virtual bool save() = 0;
-        virtual bool remove() = 0;
-        template<typename T>
-        virtual T& IModel::operator[](const std::string& col_name) = 0;
-        template<typename T>
-        virtual T& IModel::operator[](const std::string& col_name, const std::string& value) = 0;
-}
+        void find_impl(int id) {
+            static_assert(false, "find with id is not implemented.");
+        }
 
+        void find(const std::string& query) {
+            static_cast<Derived*>(this)->find_impl(query);
+        }
+
+        void find_impl(const std::string& query) {
+            static_assert(false, "find with query is not implemented.")
+        }
+
+        template <typename T>
+        T get(const std::string& col_name, T default_value) {
+            return static_cast<Derived*>(this)->get_impl(col_name, default_value);
+        }
+
+        template<typename T>
+        T get_impl(const std::string& col_name, T default_value) {
+            static_assert(false, "get is not implemented.");
+        }
+        
+
+        bool set(const std::string& col_name, const std::string& value) {
+            return static_cast<Derived*>(this)->set_impl(col_name, value);
+        }
+
+        bool set_impl(const std::string& col_name, const std::string& value) {
+            static_assert(false, "set is not implemented.");
+        }
+
+        bool save() {
+            return static_cast<Derived*>(this)->save_impl();
+        }
+
+        bool save_impl() {
+            static_assert(false, "save is not implemented.");
+        }
+        
+        bool remove() {
+            return static_cast<Derived*>(this)->remove_impl();
+        }
+
+        bool remove_impl() {
+            static_assert(false, "remove is not implemented.");
+        }
+
+        template<typename T>
+        T& IModel::operator[](const std::string& col_name) {
+            return static_cast<Derived*>(this)->operator_get_impl(col_name);
+        }
+
+        template<typename T>
+        T& operator_get_impl(const std::string& col_name) {
+            static_assert(false, "operator [] is not implemented.");
+        }
+
+        template<typename T>
+        T& IModel::operator[](const std::string& col_name, const std::string& value) {
+            return static_cast<Derived*>(this)->operator_set_impl(col_name, value);
+        }
+
+        template<typename T>
+        T& operator_set_impl(const std::string& col_name, const std::string& value) {
+            static_assert(false, "operator [] is not implemented.");
+        }
+};
 #endif /* IMODEL_HPP */
--- fleropp_fpm/include/IModelDriver.hpp
+++ fleropp_fpm/include/IModelDriver.hpp
@@ -1,14 +1,17 @@
 #ifndef IMODELDRIVER_HPP
 #define IMODELDRIVER_HPP
 
+#include <string>
+#include <unordered_map>
+
 class IModelDriver {
     public:
-        virtual bool entry_create(const std::string& query) = 0;
-        virtual unordered_map<std::string, std::string> entry_read(const std::string& query) = 0;
-        virtual bool entry_update(const std::string& query) = 0;
-        virtual bool entry_delete(const std::string& query) = 0;
+        virtual bool create_entry(const std::string& query) = 0;
+        virtual std::unordered_map<std::string, std::string> read_entry(const std::string& query) = 0;
+        virtual bool update_entry(const std::string& query) = 0;
+        virtual bool delete_entry(const std::string& query) = 0;
         virtual void connect(const std::string& username, const std::string& password, 
                              const std::string& db, const std::string& host="localhost") = 0;
-}
+};
 
 #endif /* IMODELDRIVER_HPP */
